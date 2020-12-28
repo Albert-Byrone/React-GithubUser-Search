12fa87343fb88be1ec056193bc1cb08efaf2aa60
albertbyrone <albertbyrone16777@gmail.com>
2020-12-28 21:19:24 +0300

    display the user repos

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import styled from 'styled-components';
 import { GithubContext } from '../context/context';
-import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
+// import RepoCard from './RepoCard'
+
+// import { GithubContext } from '../context/context';
+// import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
 const Repos = () => {
-  return <h2>repos component</h2>;
+  const { repos } = React.useContext(GithubContext);
+
+
+  return <section className="section">
+  <Wrapper className="section-center">
+  {/* use spread operator to pass the object props ... */}
+  { repos.map((repo)=>{
+      const{ name} = repo;
+      return <Item  key={repo.id} repo={repo}/>
+      })}
+  </Wrapper>
+</section>;
 };
+
+const Item = ({repo}) =>{
+  return <article className="item">
+  <div>
+    <h3>{ repo.name}</h3>
+    <p>{repo.description}</p>
+  </div>
+</article>
+} 
+
 
 const Wrapper = styled.div`
   display: grid;
