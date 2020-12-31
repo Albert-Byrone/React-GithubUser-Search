975816e84516424a2cbab1fc1005701e2f12fe92
albertbyrone <albertbyrone16777@gmail.com>
2020-12-31 21:15:57 +0300

    remove repo imports

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 import Info from "./Info";
-import Repos from "./Repos";
 import User from "./User";
 import Search from "./Search";
 import Navbar from "./Navbar";
+import RepoCard from "./RepoCard"
 
-export { Info, Repos, User, Search, Navbar };
+export { Info, User, Search, Navbar, RepoCard };
